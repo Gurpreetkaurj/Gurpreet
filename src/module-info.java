5c913b8a9feccf778b1120248d584cb657c44582
Gurpreet <gjhinger01@gmail.com>
2019-06-05 14:51:10 -0400

    testing

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Gurpreet
+ *
+ */
+module Gurpreet {
+}
---
+++ src/sample/Gur.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Gur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Gurpreet Kaur");
+		System.out.println("A00092666");
+		System.out.println("Java Development");
+
+	}
+
+}
